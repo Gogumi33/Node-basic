5c19b4c055e5a6ce23c767573647feb215d5addb
HyeongSeok Kim <king9833@korea.ac.kr>
2024-04-05 18:07:35 +0900

    error check

--- 04/server-2.js
+++ 04/server-2.js
@@ -1,4 +1,5 @@
 // 요청 객체(req) 살펴보기  (결과 비교 파일 : 04\results\server-2.js)
+// server-2,3,4,5는 변형시켜 적용.
 
 const http = require("http");
 const server = http.createServer((req, res) => {
--- 04/server-3.js
+++ 04/server-3.js
@@ -1,4 +1,4 @@
-// 응답 객체 확인하기 - 응답 헤더, 응답 본문, 응답 종료  (결과 비교 파일 : 04\results\server-3.js)
+// 응답 객체 확인하기 - 응답 헤더, 응답 본문, 응답 종료 (결과 비교 파일 : 04\results\server-3.js)
 
 const http = require("http");
 const server = http.createServer((req, res) => {
